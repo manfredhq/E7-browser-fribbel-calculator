1f54f207f83e7694b50dd5d0ccf297d0b648a00b
manfredhq <hugo.quitton@gmail.com>
2022-09-06 00:43:16 +0200

    Index.js

--- Index.js
+++ Index.js
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html>
-<body>
-    <form>
-        Username: <input type="text" name="username" /> 
-         
-        Password:<input type="text" name="password" />
-        
-        Email:<input type="text" name="email" />
-          
-        <button>submit</button>
-    </form>
-
-    <script>
-        let form = document.getElementsByTagName("form")[0];
-
-        form.onsubmit = function() {
-            // check all rules
-        };
-    </script>
-</body>
-
-</html>
+function CalculateGS(){
+    alert("test");
+}
